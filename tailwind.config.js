015bf9213511743ad00be6b662e7ef75cda699b7
melvin290 <meltauro03@gmail.com>
2025-05-02 20:38:29 +0530

    COlorchange

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         primary: '#071952',
-        secondary: '#088395',
+        secondary: '#085F6D',
         accent: '#37B7C3',
         light: '#EBF4F6',
         dark: '#1a1a1a',
